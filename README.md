0bb7aac13647e8ddfc3cb56a6242d0f0f9c5d078
XiNGRZ <chenxingyu92@gmail.com>
2015-10-14 14:32:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,86 @@
+Pull Back Layout
+==========
+
+[![](https://img.shields.io/github/tag/oxoooo/pull-back-layout.svg?style=flat-square)](https://jitpack.io/#oxoooo/pull-back-layout)
+[![](https://img.shields.io/github/license/oxoooo/pull-back-layout.svg?style=flat-square)](LICENSE)
+[![](https://img.shields.io/github/watchers/oxoooo/pull-back-layout.svg?style=flat-square)](https://github.com/oxoooo/pull-back-layout/watchers)
+[![](https://img.shields.io/github/stars/oxoooo/pull-back-layout.svg?style=flat-square)](https://github.com/oxoooo/pull-back-layout/stargazers)
+[![](https://img.shields.io/github/forks/oxoooo/pull-back-layout.svg?style=flat-square)](https://github.com/oxoooo/pull-back-layoutt/network)
+[![](https://img.shields.io/github/issues/oxoooo/pull-back-layout.svg?style=flat-square)](https://github.com/oxoooo/pull-back-layout/issues)
+
+Pull down to finish an Activity.
+
+## Download
+
+```gradle
+repositories {
+    // ...
+    maven { url "https://jitpack.io" }
+}
+
+dependencies {
+    // ... support library ...
+    // ...
+    compile 'com.github.oxoooo:pull-back-layout:1.0.0'
+}
+```
+
+## Usage
+
+You may refer to one of our apps that already using this library: [mr-mantou-android](https://github.com/oxoooo/mr-mantou-android/blob/abc2b660b3c0e6ed5c6fe5fb962e7df19ab9d8a4/app/src/main/java/ooo/oxo/mr/ViewerActivity.java#L170)
+
+1. Wraps `ooo.oxo.library.widget.PullBackLayout` around your `ImageView` or `ViewPager`:
+
+  ```xml
+  <ooo.oxo.library.widget.PullBackLayout
+      android:id="@+id/puller"
+      android:layout_width="match_parent"
+      android:layout_height="match_parent">
+
+      <android.support.v4.view.ViewPager
+          android:id="@+id/pager"
+          android:layout_width="match_parent"
+          android:layout_height="match_parent" />
+
+  </ooo.oxo.library.widget.PullBackLayout>
+  ```
+
+2. Set a callback:
+
+  ```java
+  public class ViewerActivity extends AppCompatActivity implements PullBackLayout.Callback {
+
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          /* ... */
+          puller.setCallback(this);
+      }
+
+      @Override
+      public void onPullStart() {
+          // fade out Action Bar ...
+          // show Status Bar ...
+      }
+
+      @Override
+      public void onPull(float progress) {
+          // set the opacity of the window's background
+      }
+
+      @Override
+      public void onPullCancel() {
+          // fade in Action Bar
+      }
+
+      @Override
+      public void onPullComplete() {
+          supportFinishAfterTransition();
+      }
+
+  }
+  ```
+
+## License
+
+[MIT License](LICENSE)
---
+++ library/src/main/java/ooo/oxo/library/widget/PullBackLayout.java
@@ -0,0 +1,155 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 XiNGRZ <xxx@oxo.ooo>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package ooo.oxo.library.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.FrameLayout;
+
+public class PullBackLayout extends FrameLayout {
+
+    private final ViewDragHelper dragger;
+
+    private final int minimumFlingVelocity;
+
+    @Nullable
+    private Callback callback;
+
+    public PullBackLayout(Context context) {
+        this(context, null);
+    }
+
+    public PullBackLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public PullBackLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        dragger = ViewDragHelper.create(this, 1f / 8f, new ViewDragCallback());
+        minimumFlingVelocity = ViewConfiguration.get(context).getScaledMinimumFlingVelocity();
+    }
+
+    public void setCallback(@Nullable Callback callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return dragger.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(@NonNull MotionEvent event) {
+        dragger.processTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        if (dragger.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+
+    public interface Callback {
+
+        void onPullStart();
+
+        void onPull(float progress);
+
+        void onPullCancel();
+
+        void onPullComplete();
+
+    }
+
+    private class ViewDragCallback extends ViewDragHelper.Callback {
+
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return true;
+        }
+
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            return 0;
+        }
+
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            return Math.max(0, top);
+        }
+
+        @Override
+        public int getViewHorizontalDragRange(View child) {
+            return 0;
+        }
+
+        @Override
+        public int getViewVerticalDragRange(View child) {
+            return getHeight();
+        }
+
+        @Override
+        public void onViewCaptured(View capturedChild, int activePointerId) {
+            if (callback != null) {
+                callback.onPullStart();
+            }
+        }
+
+        @Override
+        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+            if (callback != null) {
+                callback.onPull((float) top / (float) getHeight());
+            }
+        }
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            int slop = yvel > minimumFlingVelocity ? getHeight() / 6 : getHeight() / 3;
+            if (releasedChild.getTop() > slop) {
+                if (callback != null) {
+                    callback.onPullComplete();
+                }
+            } else {
+                if (callback != null) {
+                    callback.onPullCancel();
+                }
+
+                dragger.settleCapturedViewAt(0, 0);
+                invalidate();
+            }
+        }
+
+    }
+
+}
